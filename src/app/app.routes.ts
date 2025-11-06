113e2ff5df1c658e48cb85a42fb6b9b2b7b89c3f
Schinor <marcioschinor@gmail.com>
2025-11-06 13:25:11 -0300

    Criando Paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { LadingPage } from './components/lading-page/lading-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "", component: LadingPage
+  }
+];
